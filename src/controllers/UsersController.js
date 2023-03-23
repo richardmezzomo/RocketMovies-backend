9047f6cb0459fdbedb9c8886c677b06cc8643f73
RichardMezzomo <richardmezzomo1@gmail.com>
2023-03-23 19:22:40 -0300

    feat: add updated of user password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcrypt')
+const { hash, compare } = require('bcrypt')
 const AppError = require('../utils/AppError')
 
 const sqliteConnection = require('../database/sqlite')
@@ -28,7 +28,7 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body
+    const { name, email, password, old_password } = request.body
     const { id } = request.params
 
     const database = await sqliteConnection()
@@ -47,13 +47,28 @@ class UsersController {
     user.name = name
     user.email = email
 
+    if(password && !old_password) {
+      throw new AppError("You need to enter the old password to set the new password")
+    }
+
+    if(password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if(!checkOldPassword) {
+        throw new AppError("The old password does not match")
+      }
+
+      user.password = await hash(password, 8)
+    }
+
     await database.run(`
     UPDATE users SET 
     name = ?,
     email = ?,
+    password = ?,
     updated_at = ?
     WHERE id = ?`,
-    [user.name, user.email, new Date(), id]
+    [user.name, user.email, user.password, new Date(), id]
     )
 
     return response.json()
