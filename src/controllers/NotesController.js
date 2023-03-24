904ee58f82df681e7e4ef2958888fdd39cf7879e
RichardMezzomo <richardmezzomo1@gmail.com>
2023-03-24 13:28:32 -0300

    feat: add notes record

---
+++ src/controllers/NotesController.js
@@ -0,0 +1,31 @@
+const knex = require("../database/knex")
+
+class NotesController {
+  async create(request, response) {
+    const { title, description, tags} = request.body
+    const { user_id } = request.params
+
+    const [note_id] = await knex("notes").insert({
+      title,
+      description,
+      user_id
+    })
+
+    const tagsInsert = tags.map(name => {
+      return {
+        note_id,
+        user_id,
+        name
+      }
+    })
+
+    await knex("tags").insert(tagsInsert)
+
+    response.json()
+
+
+
+  }
+}
+
+module.exports = NotesController
