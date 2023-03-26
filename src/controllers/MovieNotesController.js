2999f3b386d7dbafa04efad0990560732d638b4b
richardbmezzomo <richardmezzomo1@gmail.com>
2023-03-26 20:26:52 -0300

    feat: add to note list

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -41,11 +41,21 @@ class MovieNotesController {
   async delete(request,response) {
     const { id } = request.params
 
-    await knex("notes").where({ id }).delete()
+    await knex("movie_notes").where({ id }).delete()
 
     return response.json()
   }
 
-}
+  async index(request, response) {
+    const { user_id } = request.query
+
+    const notes = await knex("movie_notes")
+    .where({ user_id })
+    .orderBy("title")
+
+      return response.json(notes)
+  }
+
+} 
 
 module.exports = MovieNotesController
