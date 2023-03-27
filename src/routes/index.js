ff59e076be416ed378ba6cf760f611e911c13e8a
richardbmezzomo <richardmezzomo1@gmail.com>
2023-03-27 16:25:49 -0300

    feat: add list of tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,14 @@ const { Router } = require("express")
 
 const usersRouter = require("./users.routes")
 const movieNotesRoutes = require("./notes.routes")
+const movieTagsRoutes = require("./tags.routes")
 
 
 const routes = Router()
 
 routes.use("/users", usersRouter)
 routes.use("/notes", movieNotesRoutes)
+routes.use("/tags", movieTagsRoutes)
+
 
 module.exports = routes 
